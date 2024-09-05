a56de89064a909e606385fec98135910e30025ad
AV25242 <arunv.vijay@gmail.com>
2024-09-05 10:32:12 -0500

    save chunk and embeddings in Azure SQL database

--- DocumentVectorPipelineFunctions/BlobTriggerFunction.cs
+++ DocumentVectorPipelineFunctions/BlobTriggerFunction.cs
@@ -1,9 +1,14 @@
 using System.ClientModel;
+using System.Data;
+using System.Globalization;
 using System.Net;
 using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Azure.Storage.Blobs;
+using Dapper;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using OpenAI.Embeddings;
@@ -19,6 +24,21 @@ public class BlobTriggerFunction(
     private readonly ILogger _logger = loggerFactory.CreateLogger<BlobTriggerFunction>();
 
     private const string AzureOpenAIModelDeploymentDimensionsName = "AzureOpenAIModelDimensions";
+    private const string SqlConnectionString = "SqlConnectionString";
+    private const string CreateDocumentTableScript = @"
+        IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'document')
+        BEGIN
+            CREATE TABLE [dbo].[document] (
+                [Id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                [ChunkId] INT NULL,
+                [DocumentUrl] VARCHAR(500) NULL,
+                [Embedding] VARBINARY(8000) NULL,
+                [ChunkText] VARCHAR(MAX) NULL,
+                [PageNumber] INT NULL
+            );
+        END";
+
+    string? managedIdentityClientId = Environment.GetEnvironmentVariable("AzureManagedIdentityClientId");
     private static readonly int DefaultDimensions = 1536;
 
     private const int MaxRetryCount = 100;
@@ -48,6 +68,7 @@ public class BlobTriggerFunction(
     private async Task HandleBlobCreateEventAsync(BlobClient blobClient)
     {
         this.embeddingDimensions = configuration.GetValue<int>(AzureOpenAIModelDeploymentDimensionsName, DefaultDimensions);
+        var connstring = configuration.GetValue<string>(SqlConnectionString);
         this._logger.LogInformation("Using OpenAI model dimensions: '{embeddingDimensions}'.", this.embeddingDimensions);
 
         this._logger.LogInformation("Analyzing document using DocumentAnalyzerService from blobUri: '{blobUri}' using layout: {layout}", blobClient.Name, "prebuilt-read");
@@ -94,32 +115,58 @@ public class BlobTriggerFunction(
         }
 
         this._logger.LogInformation("Processing list of batches in parallel, total batches: {listSize}, chunks count: {chunksCount}", listOfBatches.Count(), totalChunksCount);
-        await Parallel.ForEachAsync(listOfBatches, new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism }, async (batchChunkText, cancellationToken) =>
+
+        //check if database table exists if not create one 
+
+        this._logger.LogInformation("Create document table if it doesnot exist.");
+
+        await EnsureDocumentTableExistsAsync(connstring, CreateDocumentTableScript);
+
+        await Parallel.ForEachAsync(listOfBatches, new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism }, async (batchChunkTexts, cancellationToken) =>
         {
-            this._logger.LogInformation("Processing batch of size: {batchSize}", batchChunkText.Count());
+            this._logger.LogInformation("Processing batch of size: {batchSize}", batchChunkTexts.Count());
             //await this.ProcessCurrentBatchAsync(blobClient, cosmosDBClientWrapper, batchChunkText);
 
-            this._logger.LogInformation("Processing text: {0}", batchChunkText);
+            this._logger.LogInformation("Processing text: {0}", batchChunkTexts);
 
             this._logger.LogInformation("Generating embeddings for batch of size: '{size}'.", batchChunkTexts.Count());
 
             if (batchChunkTexts.Count() > 0)
             {
                 var embeddings = await this.GenerateEmbeddingsWithRetryAsync(batchChunkTexts);
-            }
+                this._logger.LogInformation("embeddings generated {0}", embeddings);
 
-            // Save into Azure SQL
+                if (embeddings.Count() > 0)
+                {
+                    // Save into Azure SQL
+                    this._logger.LogInformation("Begin Saving data in Azure SQL");
+
+                    for (int index = 0; index < batchChunkTexts.Count; index++)
+                    {
+                        using (IDbConnection db = new SqlConnection(connstring))
+                        {
+                            string insertQuery = @"INSERT INTO [dbo].[Document] (ChunkId, DocumentUrl, Embedding, ChunkText, PageNumber) VALUES (@ChunkId, @DocumentUrl,@Embedding,@ChunkText, @PageNumber);";
+
+                            var doc = new Document()
+                            {
+                                ChunkId = batchChunkTexts[index].ChunkNumber,
+                                DocumentUrl = blobClient.Uri.AbsoluteUri,
+                                Embedding = (embeddings[index].Vector.ToArray()).Select(f => BitConverter.GetBytes(f)).SelectMany(b => b).ToArray(),
+                                ChunkText = batchChunkTexts[index].Text,
+                                PageNumber = batchChunkTexts[index].PageNumberIfKnown,
+                            };
+
+                            var result = db.Execute(insertQuery, doc);
+                        }                                    
+                    }
+
+                    this._logger.LogInformation("End Saving data in Azure SQL");
+                }
+            }            
         });
 
         this._logger.LogInformation("Finished processing blob {name}, total chunks processed {count}.", blobClient.Name, totalChunksCount);
     }
-
-    //private async Task ProcessCurrentBatchAsync(BlobClient blobClient, CosmosDBClientWrapper cosmosDBClientWrapper, List<TextChunk> batchChunkTexts)
-    //{
-
-    //    this._logger.LogInformation("Creating Cosmos DB documents for batch of size {count}", batchChunkTexts.Count);
-    //    await cosmosDBClientWrapper.UpsertDocumentsAsync(blobClient.Uri.AbsoluteUri, batchChunkTexts, embeddings);
-    //}
 
     private async Task<EmbeddingCollection> GenerateEmbeddingsWithRetryAsync(IEnumerable<TextChunk> batchChunkTexts)
     {
@@ -164,5 +211,14 @@ public class BlobTriggerFunction(
         this._logger.LogInformation("Handling delete event for blob name {name}.", blobClient.Name);
 
         await Task.Delay(1);
+    }
+
+    private static async Task EnsureDocumentTableExistsAsync(string connectionString, string script)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            await connection.OpenAsync();
+            await connection.ExecuteAsync(script);
+        }
     }
 }
---
+++ DocumentVectorPipelineFunctions/Document.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DocumentVectorPipelineFunctions
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public int? ChunkId { get; set; }
+        public string DocumentUrl { get; set; }
+        public byte[] Embedding { get; set; }
+        public string ChunkText { get; set; }
+        public int? PageNumber { get; set; }
+    }
+
+}
